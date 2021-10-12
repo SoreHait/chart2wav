a8283cf95f6f8a71c1d1d6290fed5ddc013ff30f
SoreHait <sorehait@foxmail.com>
2021-10-12 11:56:53 +0800

    update readme

--- mixer/mixer.py
+++ mixer/mixer.py
@@ -3,11 +3,11 @@ import sys
 
 def music_mix(m1_path: str, m2_path: str, out_path: str, offset=0):
     _r = m1_path.split(".")
-    m1_type = _r[len(_r) - 1]
+    m1_type = _r[-1]
     _r = m2_path.split(".")
-    m2_type = _r[len(_r) - 1]
+    m2_type = _r[-1]
     _r = out_path.split(".")
-    out_type = _r[len(_r) - 1]
+    out_type = _r[-1]
 
     m1 = AudioSegment.from_file(m1_path, format=m1_type)
     m2 = AudioSegment.from_file(m2_path, format=m2_type)
@@ -17,7 +17,7 @@ def music_mix(m1_path: str, m2_path: str, out_path: str, offset=0):
 
     export = m1.overlay(m2, position=offset)
     export.export(out_path, format=out_type)
-    print(out_path)
+    print(f'File written to "{out_path}".')
 
 
 if __name__ == "__main__":
@@ -25,10 +25,9 @@ if __name__ == "__main__":
 
     if len(args) == 4:
         music_mix(args[1], args[2], args[3])
-        sys.exit()
 
-    if len(args) == 5:
+    elif len(args) == 5:
         music_mix(args[1], args[2], args[3], int(args[4]))
-        sys.exit()
 
-    print("m1_path, m2_path, out_path, offset=0")
+    else:
+        print('Not enough or too much args.')
