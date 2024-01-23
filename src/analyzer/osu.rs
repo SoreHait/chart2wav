01acf0366bbbf0f998c08a0889ce4b2f97fdea95
SoreHait <sorehait@foxmail.com>
2024-01-24 00:38:42 +0800

    add osu analyzer module, update readme, rename repo
    Former-commit-id: b91c2f05b23897667aae30352408682c5f97366d

--- src/analyzer/osu.rs
+++ src/analyzer/osu.rs
@@ -1,6 +1,25 @@
-use crate::util::helper;
 use crate::util::types::MixerData;
 
 pub fn analyze(content: &String) -> (i32, Vec<MixerData>) {
-    return (0, vec![]);
+    let mut mixer_data: Vec<MixerData> = vec![];
+    let mut is_hit_objects: bool = false;
+    for line in content.lines() {
+        if line.len() == 0 {
+            continue;
+        }
+        if is_hit_objects {
+            if line.starts_with("[") {
+                is_hit_objects = false;
+                continue;
+            }
+            mixer_data.push(MixerData {
+                timing: line.splitn(4, ",").collect::<Vec<_>>()[2].parse().unwrap(),
+                _type: 0,
+            })
+        }
+        if line == "[HitObjects]" {
+            is_hit_objects = true;
+        }
+    }
+    return (0, mixer_data);
 }
